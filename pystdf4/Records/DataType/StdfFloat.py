64b28f79e563846505776c90a414a6ae50677aa1
Lansus <asa_choo@hotmail.com>
2025-11-04 21:11:39 +0800

    Add default value for float and binary type data.

--- pystdf4/Records/DataType/StdfFloat.py
+++ pystdf4/Records/DataType/StdfFloat.py
@@ -54,6 +54,7 @@ class R_4(StdfFloatBase):
         super().__init__(
             code="R*4",
             description="Four byte floating point number (IEEE 754)",
+            missing_default=0.0,
         )
 
 
@@ -62,4 +63,5 @@ class R_8(StdfFloatBase):
         super().__init__(
             code="R*8",
             description="Eight byte floating point number (IEEE 754)",
+            missing_default=0.0,
         )
